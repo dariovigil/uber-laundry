db215c4ee3edf9b9efe1cc703c6a815dee23f687
Dario Vigil <dariovigil@gmail.com>
2017-11-17 10:07:11 +0100

    First iteration.

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
+const authRoute = require('./routes/auth')
+const index = require('./routes/index');
+const users = require('./routes/users');
+const mongoose = require('mongoose');
+const app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
+const dbURL = 'mongodb://localhost/uberlaundry';
+mongoose.connect(dbURL).then( () => {
+  debug(`Connected to ${dbURL}`);
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -28,10 +32,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/', authRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
